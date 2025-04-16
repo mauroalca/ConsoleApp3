3a60a940a1beec883ab0e523df81e738031ca852
UCLAB119 <UCLAB119@UCLAB119>
2025-04-16 16:54:54 -0500

    Agregar archivos de proyecto.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nota1, nota2, nota3, nota4;
+            double promedio;
+
+            Console.WriteLine("ingrese primera nota: ");
+
+            Console.WriteLine("ingrese segunda nota: ");
+
+            Console.WriteLine("ingrese tercera nota: ");
+
+            Console.WriteLine("ingrese cuarta nota: ");
+
+            promedio=( nota1*0.10 + nota2*0.20 + nota3*0.30 + nota40.4)
+
+                if (promedio>=12)
+                Console.WriteLine("el alumno aprobo")
+        }
+    }
+}
